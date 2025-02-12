b1c99f202b1009ed0a982ac524ec569a25b74656
Nicolly Ribeiro Luz <nicolly.luz@ufu.br>
2025-02-12 16:59:19 -0300

    Add files via upload

---
+++ Desafio08/Desafio08/Desafio08App.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio08App.swift
+//  Desafio08
+//
+//  Created by Turma02-18 on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Desafio08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
