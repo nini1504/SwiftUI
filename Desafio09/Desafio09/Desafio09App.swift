91f89d119b7407c903c774c228409ec37b22bac7
Nicolly Ribeiro Luz <nicolly.luz@ufu.br>
2025-02-17 14:00:51 -0300

    Add files via upload

---
+++ Desafio09/Desafio09/Desafio09App.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio09App.swift
+//  Desafio09
+//
+//  Created by Turma02-18 on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Desafio09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
